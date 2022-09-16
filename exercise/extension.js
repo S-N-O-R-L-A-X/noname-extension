6db83182988e784949240fc30423f460fe9a843c
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2022-09-16 16:09:43 +0800

    feat(exercise-002): add sleep

--- exercise/extension.js
+++ exercise/extension.js
@@ -7,7 +7,7 @@ game.import("extension", function (lib, game, ui, get, ai, _status) {
         }, help: {}, config: {}, package: {
             character: {
                 character: {
-                    "宝": ["male", "wu", 4, ["跋扈", "repojun"], []],
+                    "宝": ["male", "wu", 4, ["repojun", "睡觉"], []],
                 },
                 translate: {
                     "宝": "宝",
@@ -36,10 +36,22 @@ game.import("extension", function (lib, game, ui, get, ai, _status) {
                             },
                         },
                     },
+                    "睡觉": {
+                        enable: 'phaseUse',
+                        filter: function (event, player) {
+                            return !player.classList.contains('turnedover');
+                        },
+                        content: function () {
+                            player.recover(player.maxHp - player.hp);
+                            player.turnOver();
+                        }
+                    }
                 },
                 translate: {
                     "跋扈": "跋扈",
                     "跋扈_info": "锁定技，准备阶段开始时，你摸一张牌。出牌阶段，你可以多使用一张【杀】。",
+                    "睡觉": "睡觉",
+                    "睡觉_info": "出牌阶段时，若你的武将牌正面向上，你可以翻面，并将体力值恢复至体力上限。",
                 },
             },
             intro: "",
