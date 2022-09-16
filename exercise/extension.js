d4e9a53241708523f902df3242fd2ecbd5ab62b1
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2022-09-16 16:16:44 +0800

    feat(exercise-003): add dream talk

--- exercise/extension.js
+++ exercise/extension.js
@@ -39,19 +39,41 @@ game.import("extension", function (lib, game, ui, get, ai, _status) {
                     "睡觉": {
                         enable: 'phaseUse',
                         filter: function (event, player) {
-                            return !player.classList.contains('turnedover');
+                            return !player.isTurnedOver();
                         },
                         content: function () {
                             player.recover(player.maxHp - player.hp);
                             player.turnOver();
                         }
-                    }
+                    },
+                    "梦话": {
+                        trigger: { player: 'turnOverEnd' },
+                        filter: function (event, player) {
+                            return !player.isTurnedOver();
+                        },
+                        filterTarget: function (card, player, target) {
+                            return player != target;
+                        },
+                        content: function () {
+                            'step 0'
+                            player.chooseTarget('是否发动【梦话】，对一名角色造成一点伤害？').ai = function (target) {
+                                return -get.attitude(player, target);
+                            };
+
+                            'step 1'
+                            if (result.bool) {
+                                result.targets[0].damage();
+                            };
+                        }
+                    },
                 },
                 translate: {
                     "跋扈": "跋扈",
                     "跋扈_info": "锁定技，准备阶段开始时，你摸一张牌。出牌阶段，你可以多使用一张【杀】。",
                     "睡觉": "睡觉",
                     "睡觉_info": "出牌阶段时，若你的武将牌正面向上，你可以翻面，并将体力值恢复至体力上限。",
+                    "梦话": "梦话",
+                    "梦话_info": "当你的武将牌从背面翻至正面时，你可以对一名角色造成一点伤害。",
                 },
             },
             intro: "",
