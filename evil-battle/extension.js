44faa7819e725711f7fe4a99c183b18ff1fbbe77
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2022-09-19 23:07:01 +0800

    feat(evil-006): add caomao

--- evil-battle/extension.js
+++ evil-battle/extension.js
@@ -18,12 +18,13 @@ game.import("extension", function (lib, game, ui, get, ai, _status) {
                 const list = ['xushao', 'puyuan', 'guozhao', 'guansuo', 'zhaoxiang', 'xin_lingtong',
                     're_liuzan', "caojinyu", "wanglang", "guanning", "re_sunyi", "lvlingqi", "re_panshu",
                     "zhouyi", "re_nanhualaoxian", "liuba", "dc_jiben", "shen_jiangwei", "shen_machao",
-                    "tenggongzhu"];
+                    "tenggongzhu", "caomao"];
                 const characters = initList(list);
                 lib.storage.scene["大战七阴"] = {
                     name: "大战七阴",
                     intro: "主公可供玩家设定，其余七位ai玩家从阴间武将中随机选中一个。",
                     players: [
+                        { "name": "random", "name2": "none", "identity": "zhu", "position": 1, "hp": null, "maxHp": null, "linked": false, "turnedover": false, "playercontrol": true, "handcards": [], "equips": [], "judges": [] },
                         { "name": characters[0], "name2": "none", "identity": "fan", "position": 0, "hp": null, "maxHp": null, "linked": false, "turnedover": false, "playercontrol": false, "handcards": [], "equips": [], "judges": [] },
                         { "name": characters[1], "name2": "none", "identity": "fan", "position": 0, "hp": null, "maxHp": null, "linked": false, "turnedover": false, "playercontrol": false, "handcards": [], "equips": [], "judges": [] },
                         { "name": characters[2], "name2": "none", "identity": "fan", "position": 0, "hp": null, "maxHp": null, "linked": false, "turnedover": false, "playercontrol": false, "handcards": [], "equips": [], "judges": [] },
