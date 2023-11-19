dfc6d499aaecb50fe16e3a6f207c77d22e069ed2
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2023-11-19 10:01:25 +0800

    feat: update log and download link

--- noname/src/Views/Home/Home.tsx
+++ noname/src/Views/Home/Home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <a href="https://github.com/S-N-O-R-L-A-X/noname-extension/releases/download/%E5%A4%A7%E6%88%98%E4%B8%83%E9%98%B46.4/6.4.1.zip">点击下载最新版本</a>
+      <a href="https://github.com/S-N-O-R-L-A-X/noname-extension/releases/download/%E5%A4%A7%E6%88%98%E4%B8%83%E9%98%B46.5/6.5.1.zip">点击下载最新版本</a>
     </>
   )
 }
