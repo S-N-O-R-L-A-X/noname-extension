e79ed3ec9438eea9c1c6f3727687bc21a875301a
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2024-01-22 21:36:15 +0800

    feat(evil-266): init re_boss_luxun

--- evil-battle/extension.js
+++ evil-battle/extension.js
@@ -236,6 +236,7 @@ game.import("extension", function (lib, game, ui, get, ai, _status) {
               "re_boss_huangzhong": ["male", "shu", "6/6/4", ["re_boss_lianyu", "xinliegong", "xinpojun", "re_boss_dungong", "wangong", "re_boss_baoli"], ["zhu", "boss", "bossallowed"]],
               "re_boss_xiahouba": ["male", "shu", "6/6/4", ["re_boss_lianyu", "boss_qixiang", "baobian", "zhongyun", "jieyuan", "buqu", "boss_xiaoshou", "re_boss_baoli"], ["zhu", "boss", "bossallowed"]],
               "re_boss_kuailiangkuaiyue": ["male", "wei", "6/6/4", ["re_boss_lianyu", "drlt_qianjie", "nzry_jianxiang", "nzry_shenshi", "re_boss_moqu", "jugu", "re_boss_zhene", "re_boss_baoli"], ["zhu", "boss", "bossallowed"]],
+              "re_boss_luxun": ["male", "wu", "6/6/4", ["re_boss_lianyu", "re_boss_liannu", "qianxun", "lianying", "sijian", "tairan", "qice", "re_boss_baoli"], ["zhu", "boss", "bossallowed"]],
             },
             characterSort: {
               against7devil: {
@@ -7152,6 +7153,7 @@ game.import("extension", function (lib, game, ui, get, ai, _status) {
               "re_boss_huangzhong": "羽林统帅",
               "re_boss_xiahouba": "山河图夏侯霸",
               "re_boss_kuailiangkuaiyue": "山河图蒯良蒯越",
+              "re_boss_luxun": "山河图陆逊",
 
               // skill
               "shenhu": "神护",
