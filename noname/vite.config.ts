53a52a594e807c9d0daf2174d71d81cd1b300a0f
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2024-02-25 16:40:56 +0800

    feat: separate chunks

--- noname/vite.config.ts
+++ noname/vite.config.ts
@@ -4,5 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/noname-extension/"
+  base: "/noname-extension/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.indexOf('node_modules') !== -1) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
 })
