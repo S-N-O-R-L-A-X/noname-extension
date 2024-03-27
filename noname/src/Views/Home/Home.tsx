31f29749fce8e24359b3b8c96991aa0cf9904079
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2024-03-27 22:47:19 +0800

    feat: update download link

--- noname/src/Views/Home/Home.tsx
+++ noname/src/Views/Home/Home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <a href="https://github.com/S-N-O-R-L-A-X/noname-extension/releases/download/%E5%A4%A7%E6%88%98%E4%B8%83%E9%98%B47.2/7.2.1.zip">点击下载最新版本</a>
+      <a href="https://github.com/S-N-O-R-L-A-X/noname-extension/releases/download/%E5%A4%A7%E6%88%98%E4%B8%83%E9%98%B47.3/7.3.1.zip">点击下载最新版本</a>
     </>
   )
 }
