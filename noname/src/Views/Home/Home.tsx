89311132c38318224bdc1e25a36070448bbe72d0
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2024-09-01 23:21:06 +0800

    feat: update home page

--- noname/src/Views/Home/Home.tsx
+++ noname/src/Views/Home/Home.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
-    <>
+    <div>
       <a href="https://github.com/S-N-O-R-L-A-X/noname-extension/releases/download/%E5%A4%A7%E6%88%98%E4%B8%83%E9%98%B48.3.0/8.3.0.zip">点击下载最新版本</a>
-    </>
+      <br />
+      <span>
+        可以通过下方链接加QQ群：
+        1群群号：214685856
+        2群群号：939494042
+      </span>
+      <br />
+      <span>如果想要提bug或积极互动，欢迎加入一群，否则建议申请加入二群。</span>
+    </div>
   )
 }
