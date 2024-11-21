31215cfe114167246f39bef1ac29d22a53e4b531
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2024-11-21 21:02:49 +0800

    feat: add shanhetu_boss_haiheshang1

--- game/resources/app/extension/大战七阴/source/packages/main/character.js
+++ game/resources/app/extension/大战七阴/source/packages/main/character.js
@@ -113,6 +113,7 @@ export const character = {
 		"shanhetu_boss_taotie": ["male", "shen", "7/7/4", ["re_boss_lianyu", "boss_tanyu", "boss_cangmu", "re_boss_eyi", "boss_jicai_switch", "chouhai", "keshou", "jiaozi", "re_boss_baoli"], ["zhu", "boss", "bossallowed"]],
 		"shanhetu_boss_ganning2": ["male", "wu", "6/6/4", ["re_boss_lianyu", "latest_ol_feiyang", "qixi", "fenwei", "gzduanliang", "reluoshen", "xiehui", "mingzhe", "re_boss_baoli"], ["zhu", "boss", "bossallowed"]],
 		"shanhetu_boss_yanrou": ["male", "wei", "7/7/4", ["re_boss_lianyu", "re_boss_wushuangfangtianji", "choutao", "xiangshu", "retieji", "lieren", "re_boss_baoli"], ["zhu", "boss", "bossallowed"]],
+		"shanhetu_boss_haiheshang1": ["male", "qun", "7/7/4", ["re_boss_lianyu", "boss_qixiang", "boss_tanyu", "boss_jicai_switch", "re_boss_baoyu", "jieyuan", "re_boss_xuzhang", "xinfu_gongqing", "chouhai", "benyu", "re_boss_baoli"], ["zhu", "boss", "bossallowed"]],
 	},
 	characterIntro: {
 		"re_boss_caocao": "来源于挑战模式boss魏武大帝，只加上〖神护〗就可以大战七阴。<br> 【强度】★★★★ <br> 【亮点】综合，可玩性高",
@@ -373,6 +374,7 @@ export const character = {
 		"shanhetu_boss_taotie": "山河图饕餮",
 		"shanhetu_boss_ganning2": "山河图甘宁二号",
 		"shanhetu_boss_yanrou": "山河图阎柔",
+		"shanhetu_boss_haiheshang1": "山河图海和尚一号",
 	},
 
 }
