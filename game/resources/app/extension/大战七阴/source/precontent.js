0c85aa401843f4b1fd3cdd9054fa96559503d2e5
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2025-03-20 23:08:43 +0800

    fix: fix re_boss_shihun

--- game/resources/app/extension/大战七阴/source/precontent.js
+++ game/resources/app/extension/大战七阴/source/precontent.js
@@ -34,7 +34,7 @@ export async function precontent(config, pack) {
 
 	game.utils.checkUnforcedSkill = (skill) => {
 		const info = get.info(skill);
-		if (!info || info.charlotte || info.persevereSkill || info.hiddenSkill || info.juexingji || info.limited || (info.unique && !info.gainable)) return false;
+		if (!info || !get.is.locked(skill) || info.charlotte || info.persevereSkill || info.hiddenSkill || info.juexingji || info.limited || (info.unique && !info.gainable)) return false;
 		return true;
 	}
 }
