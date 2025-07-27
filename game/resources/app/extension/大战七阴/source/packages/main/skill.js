6ea0129b06555875fcdde8b867e729b6a1d776db
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2025-07-27 14:11:19 +0800

    feat: add equip_renwangdun

--- game/resources/app/extension/大战七阴/source/packages/main/skill.js
+++ game/resources/app/extension/大战七阴/source/packages/main/skill.js
@@ -1039,6 +1039,19 @@ export const skill = {
 				player.equip(game.createCard2('zhangba', 'spade', 2));
 			}
 		},
+		"equip_renwangdun": {
+			forced: true,
+			filter: function (event, player) {
+				return (event.name != 'phase' || game.phaseNumber == 0);
+			},
+			trigger: {
+				global: 'phaseBefore',
+				player: 'enterGame',
+			},
+			content: function () {
+				player.equip(game.createCard2('renwang', 'club', 2));
+			}
+		},
 
 		"re_boss_reborn_machao": {
 			trigger: {
@@ -9106,6 +9119,8 @@ export const skill = {
 		"re_boss_zhangmashemao_info": "锁定技，游戏开始时，将【丈八蛇矛】置入你的装备区。",
 		"equip_hanbingjian": "霜刃",
 		"equip_hanbingjian_info": "锁定技，游戏开始时，将【寒冰剑】置入你的装备区。",
+		"equip_renwangdun": "持盾",
+		"equip_renwangdun_info": "锁定技，游戏开始时，将【仁王盾】置入你的装备区。",
 
 		// fusion_shen_sunce
 		"repinghe": "冯河",
