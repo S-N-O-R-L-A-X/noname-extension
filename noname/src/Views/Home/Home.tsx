4f02c33f7feb8d7473f3afa9834d783f62d53fec
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2025-11-28 22:51:44 +0800

    feat: bump to 8.17.0

--- noname/src/Views/Home/Home.tsx
+++ noname/src/Views/Home/Home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <a href="https://github.com/S-N-O-R-L-A-X/noname-extension/releases/download/%E5%A4%A7%E6%88%98%E4%B8%83%E9%98%B48.16.0/8.16.0.zip">点击下载最新版本</a>
+      <a href="https://github.com/S-N-O-R-L-A-X/noname-extension/releases/download/%E5%A4%A7%E6%88%98%E4%B8%83%E9%98%B48.17.0/8.17.0.zip">点击下载最新版本</a>
       <br />
       <span>
         可以通过下方链接加QQ群：
