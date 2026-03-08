c1e7e58eba32d0e13c7688893a7d1ffa64824452
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2026-03-08 17:53:47 +0800

    refactor: separate into files

---
+++ game/extension/大战七阴/source/packages/main/characterSort.js
@@ -0,0 +1,58 @@
+export const sort = {
+	mode_extension_大战七阴: {
+		against7devil_boss: ["re_boss_caocao", "succubus", "re_boss_huatuo", "re_boss_zhouyu",
+			"liuxingyaodi", "re_boss_zhenji", "zhizunwudi", "luanshizhuhou", "yitongjindi", "re_nianshou",
+			"re_boss_yingzhao", "re_boss_xiangliu", "re_boss_dongzhuo", "re_boss_lvbu"],
+		against7devil_fusion: ["fusion_shen_sunce", "norecover", "fusion_xuhuang", "fusion_honglianpo",
+			"re_fusion_honglianpo", "barbarian_king", "fusion_jiaxu", "fusion_liru", "fusion_weiguojixie",
+			"fusion_shuguojixie", "fusion_shuguoyinghun", "fusion_weiguoyinghun", "fusion_shuguoyinghun2",
+			"fusion_puyuan", "fusion_shen_jiangwei", "fusion_lingtong", "fusion_liuzan", "fusion_zhuanlundizang",
+			"fusion_shen_xunyu", "fusion_shen_zhangfei", "fusion_yuantanyuanxiyuanshang"],
+		against7devil_math: ["math_caojinyu", "math_xiahoujie", "math_xushao", "math_zhangchangpu", "math_beimihu"],
+		against7devil_ex: ["ex_diaochan", "ex_yingzheng", "ex_zhaoji", "ex_baiqi", "ex_zhangyi",
+			"ex_shangyang", "ex_zhaogao", "ex_miyue", "ex_lvbuwei"],
+		against7devil_shanhetu_jiuzhou_boss: ["shanhetu_jiangdong_luzhi", "shanhetu_jiangdong_xusheng", "shanhetu_jiangdong_huangzhong",
+			"shanhetu_jiangdong_xiahouba", "shanhetu_jiangdong_kuailiangkuaiyue", "shanhetu_jiangdong_luxun", "shanhetu_jiangdong_litong",
+			"shanhetu_jiangdong_ganning", "shanhetu_jiangdong_sunquan", "shanhetu_jiangdong_zhouyu", "shanhetu_jiangdong_fuwan",
+			"shanhetu_jiangdong_huaxiong", "shanhetu_jiangdong_zhangliao", "shanhetu_jiangdong_caozhang", "shanhetu_jiangdong_zhangling",
+			"shanhetu_jiangdong_wangyi", "shanhetu_jiangdong_shen_zhaoyun", "shanhetu_jiangdong_zhangliao2",
+			"shanhetu_jiangdong_wuhu_huangzhong", "shanhetu_jiangdong_wuhu_machao", "shanhetu_jiangdong_wuhu_zhaoyun",
+			"shanhetu_jiangdong_wuhu_zhangfei", "shanhetu_jiangdong_wuhu_guanyu", "shanhetu_jiangdong_yanliangwenchou",
+			"shanhetu_jiangdong_sp_pangde", "shanhetu_jiangdong_hansui", "shanhetu_jiangdong_xiahoudun", "shanhetu_jiangdong_daxiaoqiao",
+			"shanhetu_jiangdong_dengai", "shanhetu_jiangdong_shen_zhangliao", "shanhetu_jiangdong_zhangliang",
+			"shanhetu_jiangdong_shen_sunquan", "shanhetu_jiangdong_wutugu", "shanhetu_jiangdong_yuejin", "shanhetu_jiangdong_lidian",
+			"shanhetu_jiangdong_caoxiancaohua1", "shanhetu_jiangdong_caoxiancaohua2", "shanhetu_jiangdong_yanliangwenchou2",
+			"shanhetu_jiangdong_caiyong", "shanhetu_jiangdong_jushou", "shanhetu_jiangdong_lushi", "shanhetu_jiangdong_shamoke",
+			"shanhetu_jiangdong_duanjiong", "shanhetu_jiangdong_yanrou"
+		],
+		against7devil_shanhetu_yao_boss: [
+			"shanhetu_yao_yuanshao", "shanhetu_yao_lvbu", "shanhetu_yao_huangzu", "shanhetu_yao_dongzhuo",
+			"shanhetu_yao_xusheng2", "shanhetu_yao_lvmeng", "shanhetu_yao_yingzhao", "shanhetu_yao_qiongqi",
+			"shanhetu_yao_shuishengonggong", "shanhetu_yao_hundun", "shanhetu_yao_taowu", "shanhetu_yao_taotie",
+			"shanhetu_yao_ganning2", "shanhetu_yao_haiheshang1", "shanhetu_yao_haiheshang2", "shanhetu_yao_haiheshang3",
+			"shanhetu_yao_haiheshang4", "shanhetu_yao_sanshiyou", "shanhetu_yao_yuanniu", "shanhetu_yao_xiangliujushe",
+			"shanhetu_yao_baihu", "shanhetu_yao_kuileiguowang", "shanhetu_yao_yaosengzhenjia", "shanhetu_yao_feiduan",
+			"shanhetu_yao_chijianwuzhe", "shanhetu_yao_shenlouyaoshi", "shanhetu_yao_lingji", "shanhetu_yao_gulong",
+			"shanhetu_yao_baiyannv", "shanhetu_yao_yixin", "shanhetu_yao_moluoyecha", "shanhetu_yao_xiangliujushe2"
+		],
+		against7devil_shanhetu_hanzhong: [
+			"shanhetu_hanzhong_weiyan", "shanhetu_hanzhong_zhaoyun", "shanhetu_hanzhong_zhanghe",
+			"shanhetu_hanzhong_lijue", "shanhetu_hanzhong_pangde", "shanhetu_hanzhong_lvmeng",
+			"shanhetu_hanzhong_guohuai", "shanhetu_hanzhong_wanglang", "shanhetu_hanzhong_zhuling",
+			"shanhetu_hanzhong_zhangchunhua"
+		],
+		against7devil_shanhetu_junshi_boss: ["shanhetu_junshi_jiangwei", "shanhetu_junshi_yanyan",
+			"shanhetu_junshi_huojun", "shanhetu_junshi_machao", "shanhetu_junshi_zhangfei",
+			"shanhetu_junshi_wuqiao", "shanhetu_junshi_wuyi", "shanhetu_junshi_duanjiong",
+			"shanhetu_junshi_zhujun", "shanhetu_junshi_hansui", "shanhetu_junshi_lingcao",
+			"shanhetu_junshi_shamoke", "shanhetu_junshi_liangxing", "shanhetu_junshi_chengjichengcui",
+			"shanhetu_junshi_xiahoudun", "shanhetu_junshi_xushu", "shanhetu_junshi_tenglong",
+			"shanhetu_junshi_yuejin", "shanhetu_junshi_lvmeng", "shanhetu_junshi_wenyang",
+			"shanhetu_junshi_xuzhu", "shanhetu_junshi_zhuhuan", "shanhetu_junshi_sunjian",
+			"shanhetu_junshi_shoushangdebaihu", "shanhetu_junshi_jiededaofei", "shanhetu_junshi_shibixie"
+		],
+		against7devil_shanhetu_fadong_boss: [
+			"shanhetu_fadong_liusandao"
+		],
+	}
+}
--- game/extension/大战七阴/source/packages/main/main.js
+++ game/extension/大战七阴/source/packages/main/main.js
@@ -1,11 +1,19 @@
 import { lib, game, ui, get, ai, _status } from '../../../../../noname.js'
 import { basic } from '../../basic.js';
 import { card } from './card.js';
-import { character } from './character.js';
+import { characters } from './character.js';
 import { skill } from './skill.js';
+import { intro } from './characterIntro.js';
+import { sort } from './characterSort.js';
+import { translation } from './characterTranslation.js';
+
 export const extensionDefaultPackage = async function () {
+	const character = await basic.resolve(characters);
+	const characterIntro = await basic.resolve(intro);
+	const translate = await basic.resolve(translation);
+	const characterSort = await basic.resolve(sort);
 	return {
-		character: await basic.resolve(character),
+		character: {character, characterIntro, characterSort, translate},
 		skill: await basic.resolve(skill),
 		card: await basic.resolve(card)
 	};
