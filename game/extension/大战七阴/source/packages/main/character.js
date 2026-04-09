6d1baabc15284a74418315cb99146538c0d3c5bf
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2026-04-09 22:18:25 +0800

    fix: fix incorrect info

--- game/extension/大战七阴/source/packages/main/character.js
+++ game/extension/大战七阴/source/packages/main/character.js
@@ -168,7 +168,7 @@ export const characters = {
 	"shanhetu_beiwang_yangyi": ["male", "shu", "5/5/5", ["re_boss_lianyu", "dingcuo", "oljuanxia", "xiansi", "danshou", "mengjin", "boss_tianyun", "re_boss_baoli"], ["zhu", "boss", "bossallowed"]],
 	"shanhetu_beiwang_guojia": ["male", "wei", "5/5/5", ["re_boss_lianyu", "equip_zhugeliannu", "equip_bagua", "tiandu", "yiji", "oldangxian", "huituo", "re_boss_baoli"], ["zhu", "boss", "bossallowed"]],
 	"shanhetu_beiwang_yuejin": ["male", "wei", "5/5/5", ["re_boss_lianyu", "xiaoguo", "oljiangchi", "oldangxian", "qiaoli", "re_boss_yingzi", "re_boss_baoli"], ["zhu", "boss", "bossallowed"]],
-	"shanhetu_beiwang_guanyu": ["male", "shu", "5/5/5", ["re_boss_lianyu", "re_boss_wusheng", "re_boss_mashu", "nuzhan", "yijue", "xinkuanggu", "re_boss_baoli"], ["zhu", "boss", "bossallowed"]],
+	"shanhetu_beiwang_guanyu": ["male", "wei", "5/5/5", ["re_boss_lianyu", "re_boss_wusheng", "re_boss_mashu", "nuzhan", "yijue", "xinkuanggu", "re_boss_baoli"], ["zhu", "boss", "bossallowed"]],
 	"shanhetu_beiwang_caoxiu": ["male", "wei", "5/5/5", ["re_boss_lianyu", "equip_qilingong", "qianju", "qingxi", "liegong", "jieyuan", "anjian", "re_boss_baoli"], ["zhu", "boss", "bossallowed"]],
 	"shanhetu_beiwang_wangyi": ["female", "wei", "5/5/5", ["re_boss_lianyu", "equip_qinglong", "olzhenlie", "olmiji", "shouxi", "xiangle", "re_boss_baoli"], ["zhu", "boss", "bossallowed"]],
 	"shanhetu_beiwang_gongsunzan": ["male", "qun", "5/5/5", ["re_boss_lianyu", "olsbjiaodi", "olsbbaojing", "boss_chuanyun", "oldangxian", "jiaozi", "wushen", "re_boss_baoli"], ["zhu", "boss", "bossallowed"]],
--- game/extension/大战七阴/source/packages/main/characterIntro.js
+++ game/extension/大战七阴/source/packages/main/characterIntro.js
@@ -158,11 +158,11 @@ export const intro = {
 	"shanhetu_junshi_jiededaofei": "来源于山河图军师联盟篇洛阳关内饥饿的盗匪，没有任何改动。<br>【强度】★★★★★★<br> 【亮点】攻击",
 	"shanhetu_junshi_shibixie": "来源于山河图军师联盟篇洛阳关内石辟邪，没有任何改动。<br>【强度】★★★★★<br> 【亮点】综合",
 	"shanhetu_beiwang_yangyi": "来源于山河图北望篇兖州关内杨仪，没有任何改动。<br>【强度】★★★★<br> 【亮点】综合",
-	"shanhetu_beiwang_guojia": "来源于山河图北望篇兖州关内郭嘉，没有任何改动。<br>【强度】★★★★<br> 【亮点】综合",
+	"shanhetu_beiwang_guojia": "来源于山河图北望篇兖州关内郭嘉，没有任何改动。<br>【强度】★★★★<br> 【亮点】防御",
 	"shanhetu_beiwang_yuejin": "来源于山河图北望篇兖州关内乐进，没有任何改动。<br>【强度】★★★★<br> 【亮点】综合",
-	"shanhetu_beiwang_guanyu": "来源于山河图北望篇兖州关内关羽，没有任何改动。<br>【强度】★★★★<br> 【亮点】综合",
+	"shanhetu_beiwang_guanyu": "来源于山河图北望篇兖州关内关羽，没有任何改动。<br>【强度】★★★★<br> 【亮点】攻击",
 	"shanhetu_beiwang_caoxiu": "来源于山河图北望篇兖州关内曹休，没有任何改动。<br>【强度】★★★★<br> 【亮点】综合",
-	"shanhetu_beiwang_wangyi": "来源于山河图北望篇兖州关内王异，没有任何改动。<br>【强度】★★★★<br> 【亮点】综合",
-	"shanhetu_beiwang_gongsunzan": "来源于山河图北望篇兖州关内公孙瓒，没有任何改动。<br>【强度】★★★★<br> 【亮点】综合",
-	"shanhetu_beiwang_chengyu": "来源于山河图北望篇兖州关内程昱，没有任何改动。<br>【强度】★★★★<br> 【亮点】综合"
+	"shanhetu_beiwang_wangyi": "来源于山河图北望篇兖州关内王异，没有任何改动。<br>【强度】★★★★<br> 【亮点】防御",
+	"shanhetu_beiwang_gongsunzan": "来源于山河图北望篇兖州关内公孙瓒，没有任何改动。<br>【强度】★★★★<br> 【亮点】攻击",
+	"shanhetu_beiwang_chengyu": "来源于山河图北望篇兖州关内程昱，没有任何改动。<br>【强度】★★★★<br> 【亮点】防御"
 }
