4bf0c4b9daa7ca2bb01fead207e0464d11072d81
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2026-08-20 23:33:06 +0800

    feat: add shanhetu_beiwang_zhangxiu

--- game/extension/大战七阴/source/packages/main/character.js
+++ game/extension/大战七阴/source/packages/main/character.js
@@ -190,4 +190,5 @@ export const characters = {
 	"shanhetu_beiwang_zhongyuan": ["female", "qun", "9/9/6", ["re_boss_lianyu", "clanbaozu", "olmiaoxin", "miji", "re_boss_baoli"], ["zhu", "boss", "bossallowed"]],
 	"shanhetu_beiwang_hansong": ["male", "qun", "10/10/6", ["re_boss_lianyu", "nzry_cuike", "zhengrong", "zlhuji", "re_boss_baoli"], ["zhu", "boss", "bossallowed"]],
 	"shanhetu_beiwang_zhongji": ["male", "qun", "10/10/6", ["re_boss_lianyu", "rejizhi", "zhengu", "jianying", "re_boss_baoli"], ["zhu", "boss", "bossallowed"]],
+	"shanhetu_beiwang_zhangxiu": ["male", "qun", "10/10/6", ["re_boss_lianyu", "equip_hongmianbaihuapao", "boss_taiping", "xiangle", "youlong", "re_boss_baoli"], ["zhu", "boss", "bossallowed"]],
 }
--- game/extension/大战七阴/source/packages/main/characterTranslation.js
+++ game/extension/大战七阴/source/packages/main/characterTranslation.js
@@ -200,5 +200,6 @@ export const translation = {
 	"shanhetu_beiwang_duanwei": "北望篇段煨",
 	"shanhetu_beiwang_zhongyuan": "北望篇钟媛",
 	"shanhetu_beiwang_hansong": "北望篇韩嵩",
-	"shanhetu_beiwang_zhongji": "北望篇种辑"
+	"shanhetu_beiwang_zhongji": "北望篇种辑",
+	"shanhetu_beiwang_zhangxiu": "北望篇张绣"
 }
